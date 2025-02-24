c0ae38f08c4598799df02b63c998f68b3a9e5693
Sourave halder <souravh093@gmail.com>
2025-02-25 01:08:02 +0600

    resolve the problems

---
+++ week1_task3/romanToInt.js
@@ -0,0 +1,21 @@
+function romanToInt(s) {
+    const romanMap = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    };
+    
+    let total = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (i < s.length - 1 && romanMap[s[i]] < romanMap[s[i + 1]]) {
+            total -= romanMap[s[i]];
+        } else {
+            total += romanMap[s[i]];
+        }
+    }
+    
+    return total;
+}
+
+// Test cases
+console.log(romanToInt("III")); // Output: 3
+console.log(romanToInt("LVIII")); // Output: 58
+console.log(romanToInt("MCMXCIV")); // Output: 1994
